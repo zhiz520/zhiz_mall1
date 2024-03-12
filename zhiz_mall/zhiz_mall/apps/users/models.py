c839eff3a9bab39021a3e25946dfc67fcaf05ac1
zhiz <14157877+zhi2024@user.noreply.gitee.com>
2024-03-12 21:22:52 +0800

    定义用户模型类并替系统用户模型

--- zhiz_mall/zhiz_mall/apps/users/models.py
+++ zhiz_mall/zhiz_mall/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# Define User Model Class
+class User(AbstractUser):
+    mobile = models.CharField(max_length=11, unique=True)
+
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name_plural = '用户'
