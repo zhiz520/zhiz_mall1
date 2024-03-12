30ec6848723d0bf53f4f438138fc7f9f57fb45fe
zhiz <14157877+zhi2024@user.noreply.gitee.com>
2024-03-12 19:53:40 +0800

    配置mysql

--- zhiz_mall/zhiz_mall/zhiz_mall/settings.py
+++ zhiz_mall/zhiz_mall/zhiz_mall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'zhiz_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zhiz_mall',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': '123',
     }
 }
 
