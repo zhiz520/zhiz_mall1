83087901196b1e506417a808a3c16e38faf75f4a
zhiz <14157877+zhi2024@user.noreply.gitee.com>
2024-03-22 04:23:36 +0800

    根据openid判断用户是否绑定

--- zhiz_mall/zhiz_mall/apps/oauth/urls.py
+++ zhiz_mall/zhiz_mall/apps/oauth/urls.py
@@ -1,7 +1,8 @@
-from apps.oauth.views import QQLoginView
+from apps.oauth.views import QQLoginView, OauthQQView
 from django.urls import path
 
 
 urlpattern = [
     path('/qq/authorization/', QQLoginView.as_view()),
+    path('oauth_callback/', OauthQQView.as_view()),
 ]
--- zhiz_mall/zhiz_mall/apps/oauth/views.py
+++ zhiz_mall/zhiz_mall/apps/oauth/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from QQLoginTool.QQtool import OAuthQQ
 from zhiz_mall import settings
-
+from apps.oauth.models import OAuthQQUser
+from django.contrib.auth import login
 
 # Create your views here.
 class QQLoginView(View):
@@ -18,3 +19,35 @@ class QQLoginView(View):
         )
         qq_login_url = oauth.get_qq_url()
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'login_url': qq_login_url}) # 'login_url' 是配合前端写法
+    
+
+class OauthQQView(View):
+
+    def get(self, request):
+        # get code
+        code = request.GET.get('code')
+        if code is None:
+            return JsonResponse({'code': 400, 'errmsg': 'Incomplete parameters'})
+        # 2.Get token by code
+        qq = OAuthQQ(
+            client_id=settings.QQ_CLIENT_ID,
+            client_secret=settings.QQ_CLIENT_SECRET,
+            redirect_uri=settings.QQ_REDIRECT_URI,
+            state='xxxx'
+        )
+        token = qq.get_access_token(code)
+        # Get openid by token
+        openid = qq.get_open_id(token)
+
+        # Determine if a user is bound based on the poenid
+        try:
+            qquser = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            response = JsonResponse({'code': 400, 'errmsg': 'user unbound'})
+        else:
+            login(request, qquser.user)
+            response = JsonResponse({'cdoe': 0, 'errmsg': 'ok'})
+            response.set_cookie('username', qquser.user.username)
+            return response
+
+
